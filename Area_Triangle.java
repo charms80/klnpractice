e662a2bfaa211372e062f88654aeb83d84ee8db1
Kamal Sharma <kamalbsharma@gmail.com>
2021-12-12 13:34:30 +0000

    [Week-1] Added Area Of Triangle java program

---
+++ Area_Triangle.java
@@ -0,0 +1,11 @@
+package javaexamples;
+
+public class Area_Triangle {
+    public static void main(String[] args) {
+        int b = 5;
+        int h = 10;
+        int a = (b * h)/2;
+        System.out.println("Area of triangle is: " +a);
+
+    }
+}
