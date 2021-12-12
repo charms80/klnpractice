2d1fd7674e66588f488d6f58efdab94b7c31d1aa
charmi <charmsuk99@gmail.com>
2021-12-12 12:46:19 +0000

    [Week-1] Added tow numbers sum java program

---
+++ AddTwoNumbers.java
@@ -0,0 +1,10 @@
+package javaexamples;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int a=5;
+        double b=48.3;
+        double c=a+b;
+        System.out.println("add a and b=" +c);
+    }
+}
