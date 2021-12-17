9a49c6a7473b782fc46886a408e0d58e4f236938
charmi <charmsuk99@gmail.com>
2021-12-17 13:33:03 +0000

    [week1]Leapyear java program

---
+++ LeapYear.java
@@ -0,0 +1,11 @@
+package javaexamples;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year=2028;
+        if(year%4==0)
+            System.out.println("This year is Leap year");
+        else
+            System.out.println("This year is  not Leap year");
+    }
+}
---
+++ OddEvenNumbers.java
@@ -0,0 +1,13 @@
+package javaexamples;
+
+public class OddEvenNumbers {
+    public static void main(String[] args) {
+        int num = 111;
+        if(num % 2==0)
+        System.out.println("entered num is even");
+        else{
+            System.out.println("entered number is odd");
+
+        }
+    }
+}
