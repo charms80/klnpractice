79df8d618b8eab62a6e66b49314845f05f24d98f
charmi <charmsuk99@gmail.com>
2021-12-19 11:08:28 +0000

    I have added the last name in existing GET URL file.

--- GetURL.java
+++ GetURL.java
@@ -47,5 +47,6 @@ public class GetURL {
   //Get current page URL
   String CurrentURL = driver.getCurrentUrl();
   System.out.println("My Current URL Is  : "+CurrentURL);
+  driver.findElement(By.xpath("//input[@name='lname']")).sendKeys("shah");
  }
 }
